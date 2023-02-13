747ec6f08f94cbd428698d83e0bb9fea70e8acd9
Pascal JK <pascual.santos80@gmail.com>
2023-02-13 22:59:52 +0200

    - load screen based on auth data.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flash_chat/firebase_options.dart';
+import 'package:flash_chat/view_models/base.dart';
 import 'package:flutter/material.dart';
 import 'package:flash_chat/screens/welcome_screen.dart';
 import 'package:flash_chat/screens/login_screen.dart';
@@ -15,11 +16,20 @@ void main() async {
 class FlashChat extends StatelessWidget {
   const FlashChat({super.key});
 
+  String checkAuthData() {
+    var base = BaseViewModel();
+    if (base.auth.currentUser != null) {
+      return ChatScreen.id;
+    } else {
+      return WelcomeScreen.id;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(scaffoldBackgroundColor: Colors.grey[300]),
-      initialRoute: WelcomeScreen.id,
+      initialRoute: checkAuthData(),
       routes: {
         WelcomeScreen.id: (context) => const WelcomeScreen(),
         LoginScreen.id: (context) => const LoginScreen(),
